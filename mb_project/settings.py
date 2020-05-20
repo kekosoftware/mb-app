3e6c3f45e8e415f6979548d2ac4494a073cc57d3
kekosoftware <34380101+kekosoftware@users.noreply.github.com>
2020-05-20 19:17:48 -0300

    New updates for Heroku deplyment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(tf4z8aq_-)#f51-6$2=xs4ja3554xz82yd_^#s*r5uefly&7@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
